83c2fc03b174a8e6d55ffe20526dbd8c865dbb48
Hallan Christian <hallanchristiancruz@gmail.com>
2024-10-20 23:53:15 -0300

    alterações creatina

--- src/main/java/br/unitins/tp1/creatina/dto/CreatinaRequestDTO.java
+++ src/main/java/br/unitins/tp1/creatina/dto/CreatinaRequestDTO.java
@@ -1,16 +1,17 @@
 package br.unitins.tp1.creatina.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 public record CreatinaRequestDTO(
     @NotBlank(message = "O nome não pode ser nulo.")
     String nome, 
     @NotBlank(message = "A marca não pode ser nula.")
     String marca,
-    @NotBlank(message = "A quantidade em gramas não pode ser nula.")
+    @NotNull(message = "A quantidade em gramas não pode ser nula.")
     double quantidadeEmGramas,
     @NotBlank(message = "O tipo não pode ser nulo.")
     String tipo,
-    @NotBlank(message = "O preço não pode ser nulo.")
+    @NotNull(message = "O preço não pode ser nulo.")
     double preco
 ) {}
